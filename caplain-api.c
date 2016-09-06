e733e9fa5dca319cf9b0a27bd8a7b33c0845cdfb
Kypaz <aleexbouquet@gmail.com>
2016-09-06 14:59:41 +0200

    minor change to add :emis le

--- caplain-api.c
+++ caplain-api.c
@@ -3,16 +3,20 @@
 #include <tidybuffio.h>
 #include <curl/curl.h>
 
-void print_node(TidyDoc doc, TidyNode tnod) 
+void print_node(TidyDoc doc, TidyNode tnod, int is_end_line)
 {
     TidyBuffer buf;
     tidyBufInit(&buf);
     tidyNodeGetText(doc, tnod, &buf);
-    printf("%s\n", buf.bp?(char *)buf.bp:""); 
+    if (is_end_line)
+      printf("%s\n", buf.bp?(char *)buf.bp:"");
+    else
+      printf("%s", buf.bp?(char *)buf.bp:"");
+
     tidyBufFree(&buf);
 }
 
-void handle_print_class(TidyDoc doc, TidyNode tnod, TidyAttr attr) 
+void handle_print_class(TidyDoc doc, TidyNode tnod, TidyAttr attr)
 {
     TidyNode prt_nod;
     if (tidyAttrValue(attr) == NULL) {
@@ -20,56 +24,67 @@ void handle_print_class(TidyDoc doc, TidyNode tnod, TidyAttr attr)
     }
 
     if (!strcmp(tidyAttrValue(attr), "titre")) {
-        printf("\n");        
+        printf("\n");
     }
 
     if (!strcmp(tidyAttrValue(attr), "titre")
-            || !strcmp(tidyAttrValue(attr), "donnees") 
+            || !strcmp(tidyAttrValue(attr), "donnees")
             || !strcmp(tidyAttrValue(attr), "blabla")) {
 
         prt_nod = tidyGetChild(tnod);
-        print_node(doc, prt_nod);
+        print_node(doc, prt_nod, 1);
     }
 }
 
-void handle_print_tags(TidyDoc doc, TidyNode tnod, ctmbstr name) 
+void handle_print_tags(TidyDoc doc, TidyNode tnod, ctmbstr name)
 {
     TidyNode prt_nod, prt2_nod;
 
-    if (!strcmp(name, "title")) {
-        printf("\n");
+    if (!strcmp(name, "i")) {
+
+        ctmbstr parent_name= tidyNodeGetName(tidyGetParent(tnod));
+        if (strcmp(parent_name, "b"))
+        {
+          printf("\n");
+        }
+
         prt_nod = tidyGetChild(tnod);
-        print_node(doc, prt_nod);
+        print_node(doc, prt_nod, 1);
     }
 
-    if (!strcmp(name, "i")) {
-        printf("\n");
-        prt_nod = tidyGetChild(tnod);
-        print_node(doc, prt_nod);
+    if (!strcmp(name, "b")) {
+      prt_nod = tidyGetChild(tnod);
+      int is_end_line = 1 ;
+      ctmbstr child_name = tidyNodeGetName(tidyGetNext(prt_nod));
+      if (!strcmp(child_name, "i"))
+      {
+        is_end_line = 0 ;
+      }
+      print_node(doc, prt_nod,is_end_line);
     }
 }
 
-/* Traverse the document tree */ 
+/* Traverse the document tree */
 void process_tree(TidyDoc doc, TidyNode tnod)
 {
     TidyNode child;
     for(child = tidyGetChild(tnod); child; child = tidyGetNext(child) ) {
         ctmbstr name = tidyNodeGetName(child);
         if(name) {
-            /* if it has a name, then it's an HTML tag ... */ 
+            /* if it has a name, then it's an HTML tag ... */
             TidyAttr attr;
-            handle_print_tags(doc, child, name);       
-            
-            /* walk the attribute list */ 
+            handle_print_tags(doc, child, name);
+
+            /* walk the attribute list */
             for(attr=tidyAttrFirst(child); attr; attr=tidyAttrNext(attr)) {
-                handle_print_class(doc, child, attr);       
+                handle_print_class(doc, child, attr);
             }
         }
         else {
-            /* if it doesn't have a name, then it's probably text, cdata, etc... */ 
+            /* if it doesn't have a name, then it's probably text, cdata, etc... */
             // print_node(doc, child);
         }
-        process_tree(doc, child); /* recursive */ 
+        process_tree(doc, child); /* recursive */
     }
 }
 
@@ -117,7 +132,7 @@ int iso_8859_1s_to_utf8s(char *utf8str, const char *mbstr, size_t count)
     return res;
 }
 
-/* curl write callback, to fill tidy's input buffer...  */ 
+/* curl write callback, to fill tidy's input buffer...  */
 uint write_cb(char *in, uint size, uint nmemb, TidyBuffer *out)
 {
     uint r;
@@ -147,7 +162,7 @@ int main(int argc, char **argv)
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb);
 
     tdoc = tidyCreate();
-    tidyOptSetBool(tdoc, TidyForceOutput, yes); /* try harder */ 
+    tidyOptSetBool(tdoc, TidyForceOutput, yes); /* try harder */
     tidyOptSetInt(tdoc, TidyWrapLen, 4096);
     tidySetErrorBuffer(tdoc, &tidy_errbuf);
     tidySetCharEncoding(tdoc, "UTF8");
@@ -156,20 +171,20 @@ int main(int argc, char **argv)
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &docbuf);
     err=curl_easy_perform(curl);
     if(!err) {
-        err = tidyParseBuffer(tdoc, &docbuf); /* parse the input */ 
+        err = tidyParseBuffer(tdoc, &docbuf); /* parse the input */
         if(err >= 0) {
-            err = tidyCleanAndRepair(tdoc); /* fix any problems */ 
+            err = tidyCleanAndRepair(tdoc); /* fix any problems */
             if(err >= 0) {
-                err = tidyRunDiagnostics(tdoc); /* load tidy error buffer */ 
+                err = tidyRunDiagnostics(tdoc); /* load tidy error buffer */
                 if(err >= 0) {
-                    process_tree(tdoc, tidyGetRoot(tdoc)); /* walk the tree */ 
-                    // fprintf(stderr, "%s\n", tidy_errbuf.bp); /* show errors */ 
+                    process_tree(tdoc, tidyGetRoot(tdoc)); /* walk the tree */
+                    // fprintf(stderr, "%s\n", tidy_errbuf.bp); /* show errors */
                 }
             }
         }
     }
 
-    /* clean-up */ 
+    /* clean-up */
     curl_easy_cleanup(curl);
     tidyBufFree(&docbuf);
     tidyBufFree(&tidy_errbuf);
